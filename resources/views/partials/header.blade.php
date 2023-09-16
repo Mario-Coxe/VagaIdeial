71c70f37035077ee2519ce78c2884ef2ad136f67
Mario-Coxe <cox3site59@gmail.com>
2023-09-16 18:29:52 +0100

    VagaIdeial

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header id="header" id="home">
+    <div class="container">
+        <div class="row align-items-center justify-content-between d-flex">
+          <div id="logo">
+            <!-- <a href="{{ route('home') }}"><img src="{{ asset('img/logo.png') }}" alt="" title="" /></a> -->
+            <h4>LOGO</h4>
+          </div>
+        </div>
+    </div>
+</header>
